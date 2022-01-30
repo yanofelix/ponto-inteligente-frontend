54f2862efd3fa958f67ebbf69e3ed2ddcbebe150
Yan Oliveira <yoliveira@indracompany.com>
2022-01-30 11:54:14 -0300

    cpf validator criado

--- src/app/autenticacao/cadastro-pj/components/cadastrar-pj/cadastrar-pj.component.ts
+++ src/app/autenticacao/cadastro-pj/components/cadastrar-pj/cadastrar-pj.component.ts
@@ -3,6 +3,7 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { Router } from '@angular/router';
 import { CadastroPj } from '../../models';
+import { CpfValidator } from '../../../../shared/validators';
 
 @Component({
   selector: 'app-cadastrar-pj',
@@ -28,7 +29,7 @@ export class CadastrarPjComponent implements OnInit {
       nome: ['', [Validators.required, Validators.minLength(3)]],
       email: ['', [Validators.required, Validators.email]],
       senha: ['', [Validators.required, Validators.minLength(6)]],
-      cpf: ['', [Validators.required]],
+      cpf: ['', [Validators.required, CpfValidator]],
       razaoSocial: ['', [Validators.required, Validators.minLength(5)]],
       cnpj: ['', [Validators.required]]
     });
