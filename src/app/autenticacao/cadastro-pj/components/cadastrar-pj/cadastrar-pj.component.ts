1b796d548cdccf0cfadfeb882d321421387a10e7
Yan Oliveira <yoliveira@indracompany.com>
2022-01-30 12:04:37 -0300

    cnpj validator criado

--- src/app/autenticacao/cadastro-pj/components/cadastrar-pj/cadastrar-pj.component.ts
+++ src/app/autenticacao/cadastro-pj/components/cadastrar-pj/cadastrar-pj.component.ts
@@ -4,6 +4,7 @@ import { MatSnackBar } from '@angular/material/snack-bar';
 import { Router } from '@angular/router';
 import { CadastroPj } from '../../models';
 import { CpfValidator } from '../../../../shared/validators';
+import { CnpjValidator } from 'src/app/shared/validators/cnpj.validator';
 
 @Component({
   selector: 'app-cadastrar-pj',
@@ -31,7 +32,7 @@ export class CadastrarPjComponent implements OnInit {
       senha: ['', [Validators.required, Validators.minLength(6)]],
       cpf: ['', [Validators.required, CpfValidator]],
       razaoSocial: ['', [Validators.required, Validators.minLength(5)]],
-      cnpj: ['', [Validators.required]]
+      cnpj: ['', [Validators.required, CnpjValidator]]
     });
   }
 
